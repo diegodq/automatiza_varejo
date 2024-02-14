dac2f4ea99cfcfa3c8b1e47c4faa01fb259b4b43
Diego Sousa <pediegoss@gmail.com>
2024-02-13 22:26:44 -0300

    update

--- src/services/customer/RemoveCustomerService.ts
+++ src/services/customer/RemoveCustomerService.ts
@@ -19,7 +19,6 @@ class RemoveCustomerService
 {
 	public async execute({ root, id, email, password }: RequestCustomer): Promise<string>
 	{
-
 		const customerId = Number(root);
 
 		if(await this.checkRootAccount(customerId) && id !== undefined) {
@@ -28,73 +27,98 @@ class RemoveCustomerService
 				throw new BadRequestError('do-not-registered-customer');
 
 			await this.deleteCustomer(customer);
+			return 'customer-removed';
+		}
+
+		if(await this.checkRootAccount(customerId) && id === undefined) {
+			const customer: Customer | null = await customerRepository.findOneBy({ id: Number(customerId) });
+			if(!customer)
+				throw new BadRequestError('do-not-registered-customer');
+
+			if(customer.email != email)
+				throw new BadRequestError('incorrect-email-or-password');
+
+			const passwordCheck: boolean = await compare(password, customer.password);
+			if(!passwordCheck)
+				throw new BadRequestError('incorrect-email-or-password');
+
+			const customer_id = customer.id;
+			const company_id = await this.findCompanyByUser(customer.id);
+
+			this.deleteCustomersAndCompany(customer_id, company_id);
+
+			return 'account-removed';
+		}
+
+		if(await this.checkRemoveYourSelf(customerId)) {
+			const customer: Customer | null = await customerRepository.findOneBy({ id: Number(customerId) });
+			if(!customer)
+				throw new BadRequestError('do-not-registered-customer.');
+
+			if(customer.email != email)
+				throw new BadRequestError('do-not-registered-customer.');
+
+			const passwordCheck: boolean = await compare(password, customer.password);
+			if(!passwordCheck)
+				throw new BadRequestError('incorrect-email-or-password');
+
+			this.deleteCustomer(customer);
 
 			return 'customer-removed';
 		}
 
-		// if(await this.checkRootAccount(customerId) && id === undefined) {
-		// 	const customer: Customer | null = await customerRepository.findOneBy({ id: Number(customerId) });
-		// 	if(!customer)
-		// 		throw new BadRequestError('Não há cliente cadastrado.');
+		const customer: Customer | null = await customerRepository.findOneBy({ id: Number(id) });
+		if(!customer)
+			throw new BadRequestError('do-not-registered-customer');
 
-		// 	if(customer.email != email)
-		// 		throw new BadRequestError('Senha ou email incorreto');
+		this.deleteCustomer(customer);
 
-		// 	const passwordCheck: boolean = await compare(password, customer.password);
-		// 	if(!passwordCheck)
-		// 		throw new BadRequestError('Senha ou email incorreto');
-
-		// 	this.deleteAndSendEmail(customer);
-
-		// 	return 'customer-removed';
-		// }
-
-		// if(await this.checkRemoveYourSelf(customerId)) {
-		// 	const customer: Customer | null = await customerRepository.findOneBy({ id: Number(customerId) });
-		// 	if(!customer)
-		// 		throw new BadRequestError('Não há cliente cadastrado.');
-
-		// 	if(customer.email != email)
-		// 		throw new BadRequestError('Senha ou email incorreto');
-
-		// 	const passwordCheck: boolean = await compare(password, customer.password);
-		// 	if(!passwordCheck)
-		// 		throw new BadRequestError('Senha ou email incorreto');
-
-		// 	this.deleteCustomer(customer);
-
-		// 	return 'customer-removed';
-		// }
-
-		// const customer: Customer | null = await customerRepository.findOneBy({ id: Number(id) });
-		// if(!customer)
-		// 	throw new BadRequestError('do-not-registered-customer');
-
-		// this.deleteCustomer(customer);
-
-		return 'customer-removed-not';
+		return 'customer-removed';
 	}
 
-	private async deleteAndSendEmail(customer: Customer): Promise<void>
+	private async findCompanyByUser(customer: number)
 	{
-		await this.deleteCustomer(customer);
+		const queryRunner = appDataSource.createQueryRunner();
+		await queryRunner.connect();
 
-		const accountRemoved: string = path.resolve(__dirname, '..', '..', 'notifications', 'account-removed.hbs');
-		await Mailer.sendMail({
-			from: {
-				name: 'Equipe Automatiza Varejo',
-				email: 'noreply@automatizavarejo.com.br'
-			},
-			to: {
-				name: customer.first_name,
-				email: customer.email
-			},
-			subject: 'Exclusão de conta',
-			templateData: {
-				file: accountRemoved,
-				variables: {}
-			}
+		const queryResult = await queryRunner.query(`select company.id from company
+		join customer on company.id = customer.company_id where customer.id = ?;`, [customer]);
+
+		await queryRunner.release();
+
+		let companyId = 0;
+		queryResult.forEach((item: { id: any; }) => {
+			companyId = item.id;
 		});
+
+		return companyId;
+	}
+
+	private async deleteCustomersAndCompany(customer_id: number, company_id: number): Promise<void>
+	{
+		const queryRunner = appDataSource.createQueryRunner();
+		await queryRunner.connect();
+
+		await queryRunner.query(`call remove_companies_and_users(?, ?);`, [customer_id, company_id]);
+
+		await queryRunner.release();
+
+		// const accountRemoved: string = path.resolve(__dirname, '..', '..', 'notifications', 'account-removed.hbs');
+		// await Mailer.sendMail({
+		// 	from: {
+		// 		name: 'Equipe Automatiza Varejo',
+		// 		email: 'noreply@automatizavarejo.com.br'
+		// 	},
+		// 	to: {
+		// 		name: customer.first_name,
+		// 		email: customer.email
+		// 	},
+		// 	subject: 'Exclusão de conta',
+		// 	templateData: {
+		// 		file: accountRemoved,
+		// 		variables: {}
+		// 	}
+		// });
 	}
 
 	private async deleteCustomer(customer: Customer): Promise<void>
